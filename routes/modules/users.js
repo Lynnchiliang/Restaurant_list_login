27ea798da7cab4122877052bc6aa2a1249b632d7
Lynnchiliang <shine5895@gmail.com>
2021-10-25 16:13:16 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -17,11 +17,8 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  // 取得註冊表單參數
   const { name, email, password, confirmPassword } = req.body
-  // 檢查使用者是否已經註冊
   User.findOne({ email }).then(user => {
-    // 如果已經註冊：退回原本畫面
     if (user) {
       console.log('User already exists.')
       res.render('register', {
@@ -31,7 +28,6 @@ router.post('/register', (req, res) => {
         confirmPassword
       })
     } else {
-      // 如果還沒註冊：寫入資料庫
       return User.create({
         name,
         email,
@@ -41,6 +37,11 @@ router.post('/register', (req, res) => {
         .catch(err => console.log(err))
     }
   })
+})
+
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
 })
 
 module.exports = router
