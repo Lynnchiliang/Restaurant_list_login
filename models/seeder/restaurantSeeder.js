febbd5404c4666f46dcb958a99064650707a8015
Lynnchiliang <shine5895@gmail.com>
2021-10-26 00:48:27 +0800

    modify todoSeeder

--- models/seeder/restaurantSeeder.js
+++ models/seeder/restaurantSeeder.js
@@ -1,15 +1,52 @@
-const mongoose = require("mongoose")
-const Restaurant = require("../restaurant")
-const restaurantList = require("../../restaurant.json").results
-const db = require("../../config/mongoose")
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const User = require('../user')
+const Restaurant = require('../Restaurant')
+const restaurantList = require('./restaurant.json').results
+const db = require('../../config/mongoose')
 
-db.once("open", () => {
-  console.log("running restaurantSeeder script...")
+const SEED_USER = [
+  {
+    name: 'user1',
+    email: 'user1@example.com',
+    password: '1234',
+    index: [1, 2, 3]
+  },
+  {
+    name: 'user2',
+    email: 'user2@example.com',
+    password: '1234',
+    index: [4, 5, 6]
+  }
+]
 
-  Restaurant.create(restaurantList)
+db.once('open', () => {
+  Promise.all(
+    Array.from(SEED_USER, seeder => {
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(seeder.password, salt))
+        .then(hash => User.create({
+          name: seeder.name || '吃貨',
+          email: seeder.email,
+          password: hash,
+          loginMethod: 'email'
+        }))
+        .then(user => {
+          const userRestaurantList = []
+          Promise.all(Array.from(seeder.index, i => {
+            const restaurantData = restaurantList.find(restaurant => restaurant.id === i)
+            restaurantData.userId = user._id
+            userRestaurantList.push(restaurantData)
+          }))
+          return Restaurant.create(userRestaurantList)
+        })
+    }))
     .then(() => {
-      console.log("restaurantSeeder done!")
-      db.close()
+      console.log('Done.')
+      process.exit()
     })
     .catch(err => console.log(err))
 })
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,8 +3,7 @@ const router = express.Router()
 const Restaurant = require("../../models/Restaurant")
 
 router.get("/", (req, res) => {
-  const userId = req.user._id
-  Restaurant.find({ userId })
+  Restaurant.find({})
     .lean()
     .then(restaurantsData => res.render("index", { restaurantsData }))
     .catch(err => console.log(err))
